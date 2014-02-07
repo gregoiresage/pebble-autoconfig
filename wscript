0c67086efd6797755e884e9af448ca7b11cff8c4
Grégoire Sage <gregoire.sage@astrium.eads.net>
2014-02-07 18:35:55 +0100

    first commit

---
+++ wscript
@@ -0,0 +1,26 @@
+top = '.'
+out = 'build'
+
+def options(ctx):
+    ctx.load('pebble_sdk')
+    ctx.load('autoconfig', tooldir='wtools')
+
+def configure(ctx):
+    ctx.load('pebble_sdk')
+    ctx.load('autoconfig', tooldir='wtools')
+
+def build(ctx):
+    ctx.load('pebble_sdk')
+
+    for template in ctx.path.ant_glob(['templates/*.jinja']):
+        ctx.add_manual_dependency(
+            template,
+            ctx.path.find_node('appinfo.json'))
+
+    ctx.pbl_program(
+        source=ctx.path.ant_glob(['src/**/*.c', '**/*.jinja']),
+        target='pebble-app.elf')
+
+
+    ctx.pbl_bundle(elf='pebble-app.elf',
+                   js=ctx.path.ant_glob('src/js/**/*.js'))
---
+++ wtools/autoconfig.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+import json
+
+from waflib import TaskGen, Task, Node
+from waflib.TaskGen import extension
+
+class autoconfig(Task.Task):
+	color   = 'PINK'
+
+	def run(self):
+		"""Render jinja template."""
+		from jinja2 import Environment
+		from jinja2 import FileSystemLoader
+		
+		rootdir = self.generator.path.abspath()
+		tpldir = os.path.dirname(self.inputs[0].abspath())
+		tpl = os.path.basename(self.inputs[0].abspath())
+		
+		env = Environment(loader = FileSystemLoader([rootdir, tpldir]), trim_blocks=False)
+	
+		template = env.get_template(tpl)
+		
+		f = open(self.outputs[0].abspath(), 'w')
+		f.write(template.render(self.preferences))
+		f.close()
+
+def configure(conf):
+	""" detect jinja installation """
+	#load jinja module
+	# conf.check_python_module('jinja2')
+	try:
+		from jinja2 import Environment
+		from jinja2 import FileSystemLoader
+	except Exception, e:
+		conf.fatal("Jinja template engine is not available! (" + e.message + ")")
+
+
+@extension('.jinja')
+def process_autoconfig(self, node):	
+	out = node.change_ext('')
+	out = Node.split_path(out.abspath())[-1]
+
+	appinfo_content=open('appinfo.json')
+	preferences_json=json.load(appinfo_content)['preferences']
+
+	out = self.bld.path.find_or_declare([str(out)])
+
+	tsk = self.create_task('autoconfig', [node], [out])
+	tsk.preferences = preferences_json
+
+	if out.suffix() in ['.c']:
+		self.source.append(out)
