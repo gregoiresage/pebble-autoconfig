0d0147242d6cd3aab36299dd0985add2b2131ce2
Grégoire Sage <gregoire.sage@gmail.com>
2015-08-01 23:21:53 +0200

    fixes #53

--- wtools/templates/resources/js/pebble-autoconfig.js
+++ wtools/templates/resources/js/pebble-autoconfig.js
@@ -141,16 +141,32 @@ function colorToText(iColor){
 	return "0xFFFFFF";
 }
 
+function getQueryParam(variable, default_) {
+    var query = location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (pair[0] == variable)
+            return decodeURIComponent(pair[1]);
+    }
+    return default_ || false;
+}
+
+function sendSettings() {
+    var return_to = getQueryParam('return_to', 'pebblejs://close#');
+	location.href = return_to + encodeURIComponent(JSON.stringify(PConfig.getFormValues()));
+}
+
 /* --------------------------
  * Buttons
- * --------------------------
+ * ---------------
  */
-
+ 
 document.getElementById("save-top").addEventListener("click", function (event) {
-	location.href = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(PConfig.getFormValues()));
+	sendSettings();
 });
 document.getElementById("save-bottom").addEventListener("click", function (event) {
-	location.href = 'pebblejs://close#' + encodeURIComponent(JSON.stringify(PConfig.getFormValues()));
+	sendSettings();
 });
 
 /*
